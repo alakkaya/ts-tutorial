a4ca7f073a786b3f4ea1ab5ef4a15f030df40744
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-03-22 16:04:13 +0300

    add: getter setter and protected implementations

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -1,10 +1,50 @@
 "use strict";
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// hitesh.city = "Delhi";
+// short cuts
 class User {
-    constructor(email, name) {
+    constructor(email, name, userId) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+        this._courseCount = 1;
         this.city = "";
         this.email = email;
         this.name = name;
     }
+    deleteToken() {
+        console.log("Deleting token");
+    }
+    get getAppleEmail() {
+        return this.email;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    // setters not allowed any rtuern type include void
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
 }
-const hitesh = new User("h@h.com", "Hitesh");
-hitesh.city = "Delhi";
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const hitesh = new User("h@h.com", "Hitesh", "1");
+console.log(hitesh.getAppleEmail);
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -1,13 +1,55 @@
-class User {
-  email: string;
-  name: string;
-  city: string = "";
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 
-  constructor(email: string, name: string) {
+// hitesh.city = "Delhi";
+
+// short cuts
+class User {
+  protected _courseCount = 1;
+
+  readonly city: string = "";
+  constructor(
+    public email: string,
+    public name: string,
+    private userId: string
+  ) {
     this.email = email;
     this.name = name;
   }
+  private deleteToken() {
+    console.log("Deleting token");
+  }
+
+  get getAppleEmail(): string {
+    return this.email;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  // setters not allowed any rtuern type include void
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
-const hitesh = new User("h@h.com", "Hitesh");
-hitesh.city = "Delhi";
+class SubUser extends User {
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this._courseCount = 4;
+  }
+}
+
+const hitesh = new User("h@h.com", "Hitesh", "1");
+console.log(hitesh.getAppleEmail);
