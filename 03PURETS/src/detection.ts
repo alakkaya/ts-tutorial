4826300eb5c0b2412b94dd9df21f83ea0ca0ba5a
Ali Akkaya <117384310+alakkaya@users.noreply.github.com>
2025-03-24 16:41:17 +0300

    add: Exhaustiveness checking with never

--- 03PURETS/src/detection.ts
+++ 03PURETS/src/detection.ts
@@ -91,11 +91,26 @@ interface Rectangle {
   width: number;
 }
 
-type Shape = Circle | Square;
+type Shape = Circle | Square | Rectangle;
 
 function getTrueShape(shape: Shape) {
   if (shape.kind === "circle") {
     return Math.PI * shape.radius ** 2;
   }
-  return shape.side * shape.side;
+  // return shape.side * shape.side;
+}
+
+//! Exhaustiveness checking with never
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side * shape.side;
+    case "rectangle":
+      return shape.length * shape.width;
+    default: // never will not raise an error when every possible case has been handled.
+      const _defaultforshape: never = shape;
+      return _defaultforshape;
+  }
 }
